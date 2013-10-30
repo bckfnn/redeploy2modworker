d79f3db7a7f875e9708a221b420a6906e05eebe6
bckfnn <bock.finn@gmail.com>
2013-10-30 21:38:02 +0100

    added deploy of 2 modworker.

--- src/main/java/com/mycompany/PingVerticle.java
+++ src/main/java/com/mycompany/PingVerticle.java
@@ -17,6 +17,8 @@ package com.mycompany;
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.platform.Verticle;
@@ -37,7 +39,29 @@ public class PingVerticle extends Verticle {
       }
     });
 
+    JsonObject config1 = new JsonObject().putString("address", "wqaddr1");
+
+    container.deployModule("io.vertx~mod-work-queue~2.0.0-final", config1, new Handler<AsyncResult<String>>() {
+      @Override
+      public void handle(final AsyncResult<String> event) {
+        System.out.println("deployed:" + event);
+      }
+    });
+
+    JsonObject config2 = new JsonObject().putString("address", "wqaddr2");
+
+    container.deployModule("io.vertx~mod-work-queue~2.0.0-final", config2, new Handler<AsyncResult<String>>() {
+      @Override
+      public void handle(final AsyncResult<String> event) {
+        System.out.println("deployed:" + event);
+      }
+    });
+
+
     container.logger().info("PingVerticle started");
+    container.logger().info(" class:" + this);
+    container.logger().info(" class hashcode:" + this.getClass().hashCode());
+    container.logger().info(" classloader " + this.getClass().getClassLoader());
 
   }
 }
